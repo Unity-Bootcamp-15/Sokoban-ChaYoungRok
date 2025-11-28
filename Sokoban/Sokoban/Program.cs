2e515e1db7b0e680e4439c8cca7a841d00d1d3e6
verpivia-1 <cyrr0070088@gmail.com>
2025-11-28 15:07:18 +0900

    <Feat> 플레이어 이동/리셋/강제종료 구현

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             Console.ResetColor();
@@ -13,14 +14,125 @@
 
             Console.Clear();
 
+            int x = 5;
+            int y = 10;
 
-            Console.WriteLine("Sokaban!!!");
+            Console.SetCursorPosition(5, 10);
+            Console.Write("*");
 
-            while(true) 
-            {  
+            int Infinity = 1;
+            while (Infinity<5)
+            {
+                ConsoleKeyInfo Input = (Console.ReadKey(true));
+
+                switch (Input.Key)
+                {
+                    case ConsoleKey.DownArrow:
+
+                        Console.Clear();
+                        ++y;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                        break;
+
+                    case ConsoleKey.UpArrow:
+
+                        Console.Clear();
+                        --y;
+                        if (y == 0)
+                        {
+                            y++;
+                        }
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                        break;
+
+                    case ConsoleKey.RightArrow:
+
+                        Console.Clear();
+                        ++x;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+
+                        break;
+                    case ConsoleKey.LeftArrow:
+
+                        Console.Clear();
+                        --x;
+                        if (x == 0)
+                        {
+                            x++;
+                        }
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+
+                        break;
+
+                    case ConsoleKey.F5:
+                        Console.Clear();
+
+                        x = 5;
+                        y = 10;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+
+                        break;
+
+                }
+
+                Infinity++;
+                Infinity--;
+
+                if(Input.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                /*
+                    if (Input.Key == ConsoleKey.DownArrow)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(" ");
+                        ++y;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                    }
+                    else if (Input.Key == ConsoleKey.UpArrow)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(" ");
+                        --y;
+                       if(y == 0)
+                        {
+                            y++;
+                        }
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                    }
+                    else if (Input.Key == ConsoleKey.RightArrow)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(" ");
+                        ++x;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                    }
+                    else if (Input.Key == ConsoleKey.LeftArrow)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(" ");
+                        --x;
+                       if(x==0)
+                        {
+                            x++; 
+                        }
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("*");
+                    }
+
+                */
 
             }
-
         }
     }
 }
